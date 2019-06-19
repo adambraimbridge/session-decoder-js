d55f15f9e17070eaf83ddf7be2c1d016fa2bd410
Liam Keaton <liamkeaton@gmail.com>
2019-06-19 15:23:41 +0100

    Upgrade to n-gage and circle v2
     🐿 v2.12.3

---
+++ secret-squirrel.js
@@ -0,0 +1,12 @@
+module.exports = {
+	files: {
+		allow: [],
+		allowOverrides: []
+	},
+	strings: {
+		deny: [],
+		denyOverrides: [
+			'5866ad86-183e-465d-9a45-74bb031b540a' // test/session-decoder.test.js:11
+		]
+	}
+};
